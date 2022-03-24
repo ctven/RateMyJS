a15736bf1d342c21b5c6170f5fdba17c36b0b894
Steven Cai <72951726+ctven@users.noreply.github.com>
2022-03-23 20:55:43 -0400

    Update content.js
    Protocol updated to https.

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
-if (window.location.href.includes("lsa.umich.edu/cg/cg_results.aspx") && location.protocol !== "http:") {
-    location.protocol = "http:";
+if (window.location.href.includes("lsa.umich.edu/cg/cg_results.aspx") && location.protocol !== "https:") {
+    location.protocol = "https:";
 }
